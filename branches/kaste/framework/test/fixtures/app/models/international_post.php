1c74c6d275790855516026c85744f65698889f98
kaste <kaste@a2fa5c27-f921-0410-a72c-bf682d381be0>
2007-12-12 12:38:25 +0000

    Proposed change for the interface and behavior of I18n-Columns and ActiveRecords.
    git-svn-id: http://svn.akelos.org@460 a2fa5c27-f921-0410-a72c-bf682d381be0

---
+++ branches/kaste/framework/test/fixtures/app/models/international_post.php
@@ -0,0 +1,136 @@
+<?php
+
+class InternationalPost extends I18nizedActiveRecord 
+{
+    function __construct()
+    {
+        $this->addVirtualAttributesForInternationalizedColumn('title');
+        $this->addVirtualAttributesForInternationalizedColumn('body');
+        $attributes = (array)func_get_args();
+        return $this->init($attributes);
+    }
+    
+    
+    /* Mock stuff */
+    // we don't need getAvailableLocales() => decoupled the model from the actual website
+
+    function getCurrentLocale()
+    {
+        return 'en';
+    }
+
+    /* concrete stuff; UNUSED code after 2nd refactoring */
+    function setTitles($titles)
+    {
+        $this->setInternationalizedColumnsFromArray('title',$titles);
+    }
+    
+    function getTitles()
+    {
+        return $this->getInternationalizedColumnsArray('title');
+    }
+
+    function getTitle()
+    {
+        return $this->getCurrentLocaleFromInternationalizedColumn('title');
+    }
+    
+    function setTitle($value)
+    {
+        $this->setCurrentLocaleFromInternationalizedColumn('title',$value);
+    }
+        
+}
+
+class I18nizedActiveRecord extends ActiveRecord
+{
+    var $_internationalize=false;  //turn off old implementation
+    var $_internationalizedColumns= array();
+
+    function addVirtualAttributesForInternationalizedColumn($column_name)
+    {
+        $this->_internationalizedColumns[$column_name]= null;    
+    }
+    
+    function _getAvailableLocaleForColumn($column_name)
+    {
+        if (empty($this->_internationalizedColumns[$column_name])){
+            $available_columns = array_keys($this->_columns);
+            $locale_for_column = array();
+            foreach ($available_columns as $column){
+                if (preg_match("/^([a-z]{2})_$column_name$/",$column,$matches)){
+                    $locale_for_column[]= $matches[1];
+                }
+            }
+            $this->_internationalizedColumns[$column_name] = $locale_for_column;
+        }
+        return $this->_internationalizedColumns[$column_name];
+    }
+    
+    function _isInternationalizedColumn($column_name)
+    {
+        return array_key_exists($column_name,$this->_internationalizedColumns);
+    }
+    
+    function _getSingularAttributeName($attribute_name)
+    {
+        $singular_name = AkInflector::singularize($attribute_name);
+        if ($singular_name === $attribute_name)   return false;
+        return $singular_name;
+    }
+    
+    function setAttribute($attribute, $value, $inspect_for_callback_child_method = AK_ACTIVE_RECORD_ENABLE_CALLBACK_SETTERS, $compose_after_set = true)
+    {
+        if ($this->_isInternationalizedColumn($attribute)){
+            return $this->setCurrentLocaleFromInternationalizedColumn($attribute,$value);
+        }
+        if(($attribute_singular = $this->_getSingularAttributeName($attribute)) && $this->_isInternationalizedColumn($attribute_singular)){
+            return $this->setInternationalizedColumnsFromArray($attribute_singular,$value);
+        }
+        return parent::setAttribute($attribute,$value,$inspect_for_callback_child_method,$compose_after_set);        
+    }
+    
+    function getAttribute($attribute, $inspect_for_callback_child_method = AK_ACTIVE_RECORD_ENABLE_CALLBACK_GETTERS)
+    {
+        if ($this->_isInternationalizedColumn($attribute)){
+            return $this->getCurrentLocaleFromInternationalizedColumn($attribute);
+        }
+        if(($attribute_singular = $this->_getSingularAttributeName($attribute)) && $this->_isInternationalizedColumn($attribute_singular)){
+            return $this->getInternationalizedColumnsFromArray($attribute_singular);
+        }
+        return parent::getAttribute($attribute,$inspect_for_callback_child_method);        
+    }
+    
+    function setInternationalizedColumnsFromArray($column_name,$values)
+    {
+        foreach ($values as $lang=>$value){
+            $attribute_name = $lang.'_'.$column_name;
+            $this->set($attribute_name,$value);
+        }
+    }
+    
+    function getInternationalizedColumnsArray($column_name)
+    {
+        $columns = array();
+        foreach ($this->_getAvailableLocaleForColumn($column_name) as $lang){
+            $attribute_name = $lang.'_'.$column_name;
+            $columns[$lang] = $this->get($attribute_name);
+        }
+        return $columns;
+    }
+    
+    function getCurrentLocaleFromInternationalizedColumn($column_name)
+    {
+        $attribute_name = $this->getCurrentLocale().'_'.$column_name;
+        return $this->get($attribute_name);
+    }
+
+    function setCurrentLocaleFromInternationalizedColumn($column_name,$value)
+    {
+        $attribute_name = $this->getCurrentLocale().'_'.$column_name;
+        $this->set($attribute_name,$value);
+    }
+}
+
+
+?>
---
+++ branches/kaste/framework/test/unit/lib/AkActiveRecord/_AkActiveRecord_imitating_multilingual_columns.php
@@ -0,0 +1,211 @@
+<?php
+
+defined('AK_TEST_DATABASE_ON') ? null : define('AK_TEST_DATABASE_ON', true);
+require_once(dirname(__FILE__).'/../../../fixtures/config/config.php');
+
+class test_AkActiveRecord_imitating_multilingual_columns extends AkUnitTest
+{
+    /**
+     * @var InternationalPost
+     */
+    var $InternationalPost;
+    
+    function setUp()
+    {
+    	$this->installAndIncludeModels(array('InternationalPost'=>'id,en_title,de_title'));
+    }
+    
+    function test_should_ensure_installer_didnt_added_some_columns()
+    {
+        $expected = array('id','en_title','de_title');
+        $got = array_keys($this->InternationalPost->getColumnNames());
+        $this->assertEqual($expected,$got);
+    }
+    
+    function test_should_set_the_titles()
+    {
+        $Post =& new InternationalPost();
+        $titles = array('en'=>'Something','de'=>'Etwas');
+        $Post->set('titles',$titles);
+        
+        $this->assertEqual($Post->en_title,'Something');
+        $this->assertEqual($Post->getAttribute('en_title'),$Post->en_title);
+        $this->assertEqual($Post->de_title,'Etwas');
+        $this->assertEqual($Post->getAttribute('de_title'),$Post->de_title);
+    }
+    
+    function test_should_save_titles_in_multiple_columns()
+    {
+        $titles = array('en'=>'Something','de'=>'Etwas');
+        $Post =& new InternationalPost(array('titles'=>$titles));
+        $Post->save();
+        
+        $Reloaded = $this->InternationalPost->find($Post->getId());
+        
+        $post_attributes     = $Post->getAttributes();
+        $reloaded_attributes = $Reloaded->getAttributes();
+        unset($post_attributes['id']);
+        unset($reloaded_attributes['id']);
+        $this->assertEqual($post_attributes,$reloaded_attributes);
+    }
+    
+    function test_should_get_the_titles_as_an_array()
+    {
+        $titles = array('en'=>'Something','de'=>'Etwas');
+        $Post =& new InternationalPost(array('titles'=>$titles));
+        $Post->save();
+        
+        $Reloaded = $this->InternationalPost->find($Post->getId());
+
+        $loaded_titles = $Reloaded->getTitles();
+        $this->assertEqual($loaded_titles,$titles);
+    }
+
+    function test_should_get_the_current_title_according_to_the_locale()
+    {
+        $titles = array('en'=>'Something','de'=>'Etwas');
+        $Post =& new InternationalPost(array('titles'=>$titles));
+        $Post->save();
+        
+        $Reloaded = $this->InternationalPost->find($Post->getId());
+        
+        $current_lang = $this->InternationalPost->getCurrentLocale();
+        $current_title = $Reloaded->getTitle();
+        $this->assertEqual($current_title,$titles[$current_lang]);
+        
+    }
+    
+    function test_should_set_only_the_current_title_according_to_the_locale()
+    {
+        $current_lang = $this->InternationalPost->getCurrentLocale();
+        $Post =& new InternationalPost();
+        $Post->set('title','Something');
+        
+        $this->assertEqual($Post->get('en_title'),'Something');
+    }
+    
+    /* 1st refactoring */
+    function test_should_set_international_columns_array()
+    {
+        $titles = array('en'=>'Something','de'=>'Etwas');
+        $Post =& new InternationalPost();
+        $Post->setInternationalizedColumnsFromArray('title',$titles);
+        
+        $this->assertEqual($Post->en_title,'Something');
+        $this->assertEqual($Post->getAttribute('en_title'),$Post->en_title);
+        $this->assertEqual($Post->de_title,'Etwas');
+        $this->assertEqual($Post->getAttribute('de_title'),$Post->de_title);
+    }
+    
+    function test_should_ignore_locales_for_which_we_dont_have_columns()
+    {
+        $titles = array('es'=>'qu','jp'=>'haiku');
+        $Post =& new InternationalPost();
+        $Post->setInternationalizedColumnsFromArray('title',$titles);
+        $this->assertFalse(isset($Post->es_title));
+        $this->assertNull($Post->getAttribute('es_title'));
+        $this->assertNull($Post->getAttribute('jp_title'));
+    }
+    
+    function test_should_save_internationalized_column_in_multiple_column()
+    {
+        $titles = array('en'=>'Something','de'=>'Etwas');
+        $Post =& new InternationalPost();
+        $Post->setInternationalizedColumnsFromArray('title',$titles);
+        $Post->save();
+        
+        $Reloaded =& $this->InternationalPost->find($Post->getId());
+
+        $post_attributes     = $Post->getAttributes();
+        $reloaded_attributes = $Reloaded->getAttributes();
+        unset($post_attributes['id']);
+        unset($reloaded_attributes['id']);
+        $this->assertEqual($post_attributes,$reloaded_attributes);
+    }
+
+    function test_should_get_all_internationalized_columns_in_one_array()
+    {
+        $titles = array('en'=>'Something','de'=>'Etwas');
+        $Post =& new InternationalPost();
+        $Post->setInternationalizedColumnsFromArray('title',$titles);
+        $Post->save();
+        
+        $Reloaded =& $this->InternationalPost->find($Post->getId());
+        
+        $reloaded_titles = $Reloaded->getInternationalizedColumnsArray('title');
+        $this->assertEqual($reloaded_titles,$titles);
+    }
+    
+    function test_should_get_the_current_locale_of_a_internationalized_column()
+    {
+        $titles = array('en'=>'Something','de'=>'Etwas');
+        $Post =& new InternationalPost();
+        $Post->setInternationalizedColumnsFromArray('title',$titles);
+        $Post->save();
+        
+        $Reloaded =& $this->InternationalPost->find($Post->getId());
+        $current_locale = $this->InternationalPost->getCurrentLocale();
+        $title = $Reloaded->getCurrentLocaleFromInternationalizedColumn('title');
+        $this->assertEqual($title,$titles[$current_locale]);        
+    }
+    
+    function test_should_set_the_column_according_to_the_current_locale_setting()
+    {
+        $current_locale = $this->InternationalPost->getCurrentLocale();
+        $Post =& new InternationalPost();
+        $Post->setCurrentLocaleFromInternationalizedColumn('title','Something');
+        
+        $this->assertEqual($Post->get('en_title'),'Something');
+        
+    }
+
+    /* 2nd refactoring */
+    function test_should_ensure_that_we_have_a_internationalized_column()
+    {
+        $this->InternationalPost->addVirtualAttributesForInternationalizedColumn('title');
+        $this->assertFalse(empty($this->InternationalPost->_internationalizedColumns));
+    }
+    
+    function test_should_cache_available_locales_for_each_column()
+    {
+        $this->assertTrue (empty($this->InternationalPost->_internationalizedColumns['title']));
+
+        $available_locale = array('en','de');
+        $this->assertEqual($this->InternationalPost->_getAvailableLocaleForColumn('title'),$available_locale);
+        $this->assertFalse(empty($this->InternationalPost->_internationalizedColumns['title']));
+    }
+    
+    function test_should_singularize_attribute_names()
+    {
+        $this->assertEqual($this->InternationalPost->_getSingularAttributeName('bodies'),'body');
+        $this->assertFalse($this->InternationalPost->_getSingularAttributeName('body'));
+    }
+    
+    function test_automatic_setters_for_internationalized_columns()
+    {
+        $this->installAndIncludeModels(array('InternationalPost'=>'id,en_title,de_title,en_body,de_body'));
+        $this->InternationalPost->addVirtualAttributesForInternationalizedColumn('body');
+        $this->assertFalse(empty($this->InternationalPost->_internationalizedColumns));
+        
+        $available_locale = array('en','de');
+        $this->assertEqual($this->InternationalPost->_getAvailableLocaleForColumn('body'),$available_locale);
+        
+        $Post =& new InternationalPost();
+        $Post->set('body','Something');
+        $this->assertEqual($Post->en_body,'Something');
+        $this->assertEqual($Post->get('body'),'Something');
+
+        $titles = array('en'=>'Something','de'=>'Etwas');
+        $Post =& new InternationalPost();
+        $Post->set('bodies',$titles);
+        $this->assertEqual($Post->en_body,'Something');
+        $this->assertEqual($Post->get('body'),'Something');
+        
+    }
+    
+    
+}
+
+ak_test('test_AkActiveRecord_imitating_multilingual_columns',true);
+
+?>
