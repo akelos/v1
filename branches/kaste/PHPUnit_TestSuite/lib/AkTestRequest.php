d1327074ea608a47559a0f9d16f640320f2ff36b
kaste <kaste@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-04-04 20:20:14 +0000

    Implemented AkTestRequest.
    git-svn-id: http://svn.akelos.org@536 a2fa5c27-f921-0410-a72c-bf682d381be0

---
+++ branches/kaste/PHPUnit_TestSuite/lib/AkTestRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+class AkTestRequest extends AkRequest 
+{
+
+    static function createInstance($method,$params)
+    {
+        $Request = new AkTestRequest();
+        $Request->addParamsToRequest($params);
+        $Request->setRequestMethod($method);
+        return $Request;
+    }
+    
+    function addParamsToRequest($params)
+    {
+        foreach ($params as $key=>$value){
+            $this->_addParam($key,$value);
+            $this->_request[$key] = $value;
+        }
+    }
+    
+    function setRequestMethod($method)
+    {
+        $this->_requestedMethod = $method;
+    }
+    
+    // mocked
+    function getMethod()
+    {
+        return $this->_requestedMethod;
+    }
+    
+}
+
+?>
