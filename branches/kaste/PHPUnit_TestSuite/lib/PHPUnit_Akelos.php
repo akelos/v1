d5f7880bb22734192b8c1ca8491778c9b410c65f
kaste <kaste@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-05-22 18:13:00 +0000

    Refactored autoload logic and added Installers and Models to the include path
    git-svn-id: http://svn.akelos.org@596 a2fa5c27-f921-0410-a72c-bf682d381be0

--- branches/kaste/PHPUnit_TestSuite/lib/PHPUnit_Akelos.php
+++ branches/kaste/PHPUnit_TestSuite/lib/PHPUnit_Akelos.php
@@ -1,9 +1,11 @@
 <?php
+define('AK_TEST_DATABASE_ON',true);
+PHPUnit_Akelos_autoload::ensureConfigFileLoaded();
+
 define('AK_PHPUNIT_TESTSUITE_LIB',dirname(__FILE__));
 define('AK_PHPUNIT_TESTSUITE_BASE',dirname(dirname(__FILE__)));
-define('AK_TEST_DATABASE_ON',true);
+define('AK_PHPUNIT_TESTSUITE_FIXTURES',AK_PHPUNIT_TESTSUITE_BASE.DS.'tests'.DS.'fixtures');
 
-PHPUnit_Akelos_autoload::ensureConfigFileLoaded();
 spl_autoload_register(array('PHPUnit_Akelos_autoload','__autoload'));
 
 class PHPUnit_Akelos_autoload
@@ -25,24 +27,51 @@ class PHPUnit_Akelos_autoload
         }
         if (preg_match('/^(.*)Controller$/',$classname,$matches)){
             $controller_file_name = AkInflector::underscore($classname).'.php';
-            $filename = AK_PHPUNIT_TESTSUITE_BASE.DS.'tests'.DS.'fixtures'.DS.$controller_file_name;
-            if (is_file($filename)){
-                require_once $filename;
-                return true;
-            }
-            $filename = AK_CONTROLLERS_DIR.DS.$controller_file_name;
-            if (is_file($filename)){
-                require_once $filename;
-                return true;
-            }
-            $filename = AK_BASE_DIR.DS.'app'.DS.'controllers'.DS.$controller_file_name;
-            if (is_file($filename)){
+            $filename = self::searchFileInIncludePath(self::CONTROLLER_FOLDERS(),$controller_file_name);
+            if ($filename){
                 require_once $filename;
                 return true;
             }
             return false;            
         }
+        if (preg_match('/^(.*)Installer$/',$classname,$matches)){
+            $installer_file_name = AkInflector::underscore($classname).'.php';
+            if ($filename = self::searchFileInIncludePath(self::INSTALLER_FOLDERS(),$installer_file_name)){
+                require_once $filename;
+                return true;
+            }
+            return false;
+        }
+        $model_file_name = AkInflector::underscore($classname).'.php';
+        if ($filename = self::searchFileInIncludePath(self::MODEL_FOLDERS(),$model_file_name)){
+            require_once $filename;
+            return true;
+        }
         return false; 
+    }
+    
+    static function CONTROLLER_FOLDERS()
+    {
+        return array(AK_PHPUNIT_TESTSUITE_FIXTURES,AK_CONTROLLERS_DIR,AK_BASE_DIR.DS.'app'.DS.'controllers');
+    }
+    
+    static function INSTALLER_FOLDERS()
+    {
+        return array(AK_PHPUNIT_TESTSUITE_FIXTURES,AK_APP_DIR.DS.'installers',AK_BASE_DIR.DS.'app'.DS.'installers');
+    }
+
+    static function MODEL_FOLDERS()
+    {
+        return array(AK_PHPUNIT_TESTSUITE_FIXTURES,AK_MODELS_DIR,AK_BASE_DIR.DS.'app'.DS.'models');
+    }
+    
+    static function searchFileInIncludePath($folders,$filename)
+    {
+        foreach ($folders as $folder){
+            $full_filename = $folder.DS.$filename;
+            if (is_file($full_filename)) return $full_filename;
+        }
+        return false;
     }
     
     static function ensureConfigFileLoaded()
