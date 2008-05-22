3f142835a807f059ebe56f5fc0cb6a6750cfae2b
kaste <kaste@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-05-22 18:18:43 +0000

    Added the main script to run tests.
    This script includes the autoloader.
    It then creates a TestSuite on-the-fly and includes any given file and/or folder. A leading underscore excludes the file or subfolder.
    Finally it runs the TestSuite.
    
    
    git-svn-id: http://svn.akelos.org@609 a2fa5c27-f921-0410-a72c-bf682d381be0

---
+++ branches/kaste/PHPUnit_TestSuite/scripts/test_runner.php
@@ -0,0 +1,121 @@
+<?php
+require_once dirname(dirname(__FILE__)).'/lib/PHPUnit_Akelos.php';
+require_once 'PHPUnit/Framework.php';
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+class PHPUnit_TestRunner
+{
+    private $test_files = array();
+    private $test_suite;
+    private $options = array('verbose'=>false);
+    
+    static function main($args=null)
+    {
+        if (!$args){
+            global $argv;
+            $args = $argv;
+        }
+        
+        $self = new PHPUnit_TestRunner($args);
+        $self->run()->wasSuccessful() ? exit(0) : exit(1);
+    }
+    
+    function __construct($args)
+    {
+        $this->test_suite = $this->createTestSuite($this->getMainName());
+        $this->parseArgs($args);    
+    }
+    
+    function getMainName()
+    {
+        $name = substr(AK_BASE_DIR,strrpos(AK_BASE_DIR,DS)+1);
+        return AkInflector::humanize($name,'all').' - Unit Tests';
+    }
+    
+    /**
+     * @return PHPUnit_Framework_TestSuite
+     */
+    function createTestSuite($name)
+    {
+        $suite = new PHPUnit_Framework_TestSuite();
+        $suite->setName($name);
+        return $suite;
+    }
+    
+    
+    /**
+     * @return PHPUnit_Framework_TestSuite
+     */
+    function suite()
+    {
+        return $this->test_suite;
+    }
+    
+    function parseArgs($args)
+    {
+        while (count($args) > 0){
+            $arg = array_shift($args);
+            if ($this->tryToAddToSuite($this->suite(),$arg)) continue;
+            else switch ($arg){
+                case '-v':
+                    $this->options['verbose'] = true;
+                    break;
+                case '-?':
+                    $this->drawHelp();
+                    break;
+            }
+        }
+    }
+    
+    function tryToAddToSuite(&$suite,$file)
+    {
+        if (substr($file,0,1)=='_'){
+            return false;
+        }elseif (is_file($file)){
+            $suite->addTestFile((string)$file);
+        }elseif (is_dir($file)){
+            $suite->addTestSuite($this->createSuiteForDirectory((string)$file));
+        }else{
+            return false;
+        }
+    }
+    
+    function createSuiteForDirectory($path)
+    {
+        $suite = $this->createTestSuite(AkInflector::humanize($path));
+        $files = new RecursiveDirectoryIterator($path);
+        foreach ($files as $file){
+            $this->tryToAddToSuite($suite,$file);
+        }
+        return $suite;
+    }
+    
+    function run()
+    {
+        return PHPUnit_TextUI_TestRunner::run($this->suite(),array('verbose'=>$this->options['verbose']));
+    }
+    
+    function drawHelp()
+    {
+        echo <<<BANNER
+Usage:
+
+test_runner [-v] [tests/test-suites/folders]
+   -v   verbose
+   -?   this help
+
+This script creates TestSuites on-the fly and runs them. It will exclude filenames or folders which start with an underscore.
+
+BANNER;
+        exit;
+        
+    }
+}
+
+#array_push($argv,'../ExamplesTestSuite.php');
+#array_push($argv,'../Examples');
+#array_push($argv,'-v');
+
+array_shift($argv);
+PHPUnit_TestRunner::main($argv);
+?>
