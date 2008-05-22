06fad9b7fb7222a053fd0c786b7d530571a8c4c1
kaste <kaste@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-05-22 18:10:37 +0000

    Added an example for the Routing-TestCase
    git-svn-id: http://svn.akelos.org@590 a2fa5c27-f921-0410-a72c-bf682d381be0

---
+++ branches/kaste/PHPUnit_TestSuite/tests/examples/RoutingTest.php
@@ -0,0 +1,37 @@
+<?php
+require_once preg_replace('|(tests\\'.DIRECTORY_SEPARATOR.'.*$)|','lib'.DIRECTORY_SEPARATOR.'PHPUnit_Akelos.php',__FILE__);
+
+class RoutingTestExample extends PHPUnit_Routing_TestCase 
+{
+    // if you dont setUp() anything the default map in your config/routes.php will be loaded
+    // ->useMap($map_file) to load a different one
+    
+    function testStandardRoute()
+    {
+        $this->get('blog/index');
+        $this->assertController('blog');
+        $this->assertAction('index');
+    }
+    
+    function test404()
+    {
+        $this->get('should/not/match/anything/or');
+        $this->assert404();
+    }
+    
+    function testAddAndTestANewRoute()
+    {
+        $this->instantiateRouter();
+        $this->connect('/:artist/:album',array('controller'=>'artist','action'=>'list'));
+
+        $this->get('autechre/quaristice')->resolvesTo('autechre','quaristice','artist','list');
+        
+        $this->get('boys/girls');
+        $this->assertController('artist');
+        $this->assertAction('list');
+        $this->assertArtist('boys');
+        $this->assertAlbum('girls');
+    }
+    
+}
+?>
