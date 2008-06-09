95b915a6bffbfde7373cf982b79c5a67fe98a279
kaste <kaste@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-06-09 00:10:48 +0000

    Moved the tests to tests/lib.
    Mainly because now we can simply use the run_test-script on this folder to run all tests.
    
    
    git-svn-id: http://svn.akelos.org@655 a2fa5c27-f921-0410-a72c-bf682d381be0

--- plugins/PHPUnit_TestSuite/tests/lib/ExamplesTestSuite.php
+++ plugins/PHPUnit_TestSuite/tests/lib/ExamplesTestSuite.php
@@ -9,7 +9,7 @@ class ExamplesTestSuite extends PHPUnit_Framework_TestSuite
         $test_folder = strtolower(substr($class_name,0,-9));
         
         $suite = new ExamplesTestSuite(AkInflector::titleize($class_name));
-        $path = dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.$test_folder;
+        $path = dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR.$test_folder;
         
         $directory = new PHPUnit_Runner_IncludePathTestCollector(array($path));
         $suite->addTestFiles($directory->collectTests());
