dc654736ae9c938e7d2cbda0691cf79252fca85f
kaste <kaste@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-06-27 17:37:58 +0000

    Added copyright headers to the class files.
    git-svn-id: http://svn.akelos.org@735 a2fa5c27-f921-0410-a72c-bf682d381be0

--- branches/new-router/test/unit2/AkRouter/lib/Route.php
+++ branches/new-router/test/unit2/AkRouter/lib/Route.php
@@ -1,4 +1,24 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+// +----------------------------------------------------------------------+
+// | Akelos Framework - http://www.akelos.org                             |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2002-2008, Akelos Media, S.L.  & Bermi Ferrer Martinez |
+// | Released under the GNU Lesser General Public License, see LICENSE.txt|
+// +----------------------------------------------------------------------+
+
+/**
+ * Native PHP URL rewriting for the Akelos Framework.
+ * 
+ * @package ActionController
+ * @subpackage Router
+ * @author Kaste <thdzDOTx a.t gm x N_et>
+ * @copyright Copyright (c) 2002-2008, Akelos Media, S.L. http://www.akelos.org
+ * @license GNU Lesser General Public License <http://www.gnu.org/copyleft/lesser.html>
+ */
+
+
 require_once 'VariableSegment.php';
 require_once 'WildcardSegment.php';
 
