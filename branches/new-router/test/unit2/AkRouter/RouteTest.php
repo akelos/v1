63d389a35045d19b616e081e6942a9310c8c2fb0
kaste <kaste@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-06-27 17:22:17 +0000

    Added a bunch of tests for WildcardSegments concerning requirements, compulsory-setting and combinations with static and variable-segments.
    git-svn-id: http://svn.akelos.org@709 a2fa5c27-f921-0410-a72c-bf682d381be0

--- branches/new-router/test/unit2/AkRouter/RouteTest.php
+++ branches/new-router/test/unit2/AkRouter/RouteTest.php
@@ -156,7 +156,6 @@ class RouteTest extends Route_TestCase
         $this->withRoute('/person/:name/:age',array('controller'=>'person'));
         
         $this->urlize(array('controller'=>'author'))->returnsFalse();
-        
     }
     
     function testWildcardSegmentImplicitOptional()
@@ -166,6 +165,64 @@ class RouteTest extends Route_TestCase
         $this->get('/set')              ->matches();
         $this->get('/set/this')         ->matches(array('options'=>array('this')));
         $this->get('/set/this/and/that')->matches(array('options'=>array('this','and','that')));
+    }
+    
+    function testWilcardSegmentAtTheBeginning()
+    {
+        $this->withRoute('/*parameters');
+        
+        $this->get('/unknown')    ->matches(array('parameters'=>array('unknown')));
+        $this->get('/unknown/url')->matches(array('parameters'=>array('unknown','url')));
+    }
+    
+    function testWildcardSegmentAtTheBeginningStaticFollowing()
+    {
+        $this->withRoute('/*parameters/set');
+        
+        $this->get('/set')->matches();
+        $this->get('/style=custom/set')->matches(array('parameters'=>array('style=custom')));
+    }
+    
+    function testWildcardSegmentsSurroundedByStatics()
+    {
+        $this->withRoute('/set/*options/now');
+        
+        $this->get('/set/this/now')->matches(array('options'=>array('this')));
+        $this->get('/set/now')     ->matches();
+    }
+    
+    function testCompulsoryWildcardSegment()
+    {
+        $this->withRoute('/set/*options/now',array('options'=>COMPULSORY));
+        
+        $this->get('/set/this/now')->matches(array('options'=>array('this')));
+        $this->get('/set/now')->doesntMatch();
+    }
+    
+    function testVariableSegmentFollowedByWildcardSegment()
+    {
+        $this->withRoute(':controller/*options');
+        
+        $this->get('/admin')->matches(array('controller'=>'admin'));
+        $this->get('/admin/style=blue')->matches(array('controller'=>'admin','options'=>array('style=blue')));
+    }
+    
+    function testWildcardSegmentWithRequirements()
+    {
+        $this->withRoute('/numbers/*numbers',array(),array('numbers'=>'[0-9]+'));
+        
+        $this->get('/numbers')->matches();
+        $this->get('/numbers/12/345/6/789')->matches(array('numbers'=>array('12','345','6','789')));
+        $this->get('/numbers/12/stop/789') ->doesntMatch();
+    }
+    
+    function testCompulsoryWildcardSegmentWithRequirements()
+    {
+        $this->withRoute('/numbers/*numbers',array('numbers'=>COMPULSORY),array('numbers'=>'[0-9]+'));
+        
+        $this->get('/numbers')->doesntMatch();
+        $this->get('/numbers/12/345/6/789')->matches(array('numbers'=>array('12','345','6','789')));
+        $this->get('/numbers/12/stop/789') ->doesntMatch();
     }
     
     function _testRegex()
