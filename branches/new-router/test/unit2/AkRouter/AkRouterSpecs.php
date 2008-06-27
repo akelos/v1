8b3cc0745ea4b9a46255948dced2f8451e5c119c
kaste <kaste@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-06-27 17:03:51 +0000

    Enabled reciprocity-checks in AkRouterSpecs.
    git-svn-id: http://svn.akelos.org@673 a2fa5c27-f921-0410-a72c-bf682d381be0

--- branches/new-router/test/unit2/AkRouter/AkRouterSpecs.php
+++ branches/new-router/test/unit2/AkRouter/AkRouterSpecs.php
@@ -6,6 +6,7 @@ class AkRouterSpecs extends PHPUnit_Routing_TestCase
     function setUp()
     {
         $this->instantiateRouter();
+        $this->testReciprocity();
     }
     
     function testShowsTheTestAPI()
