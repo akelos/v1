a3de1f6f51c2227c8a86d19397b31afc21601d38
kaste <kaste@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-07-10 15:42:52 +0000

    You can ask AkRouterHelper about the generated functions with AkRouterHelper::getDefinedFunctions.
    git-svn-id: http://svn.akelos.org@820 a2fa5c27-f921-0410-a72c-bf682d381be0

--- branches/new-router/lib/AkRouter/AkRouterHelper.php
+++ branches/new-router/lib/AkRouter/AkRouterHelper.php
@@ -3,48 +3,36 @@
 class AkRouterHelper
 {
 
-    function generateHelperFunctionsFor($name,AkRoute $Route)
+    private static $defined_functions = array();
+    
+    static function getDefinedFunctions()
+    {
+        return self::$defined_functions;    
+    }
+    
+    static function generateHelperFunctionsFor($name,AkRoute $Route)
     {
         $names_array_as_string = var_export($Route->getNamesOfDynamicSegments(),true);
         $names_array_as_string = str_replace(array("\n","  "),'',$names_array_as_string);
-        self::generateUrlFunction($name,$names_array_as_string);
-        self::generatePathFunction($name,$names_array_as_string);
-    }
-    
-    private static function generateUrlFunction($name,$excluded_params_as_string)
-    {
-        $function_name = $name.'_url';
-        if (function_exists($function_name)) return;
-        $code = <<<BANNER
-function $function_name(\$params=array())
-{
-    \$url_writer = AkUrlWriter::getInstance();
-    \$my_params = array(
-        'use_named_route'=>'$name',
-        'skip_old_parameters_except'=>$excluded_params_as_string
-    );
-    \$params = array_merge(\$my_params,\$params);
-    return \$url_writer->urlFor(\$params);    
-}
-
-BANNER;
-        #echo $code;
-        eval($code);
-        return $code;
         
+        self::generateFunction($name,'url',$names_array_as_string);
+        self::generateFunction($name,'path',$names_array_as_string,"'only_path'=>true");
     }
     
-    private static function generatePathFunction($name,$excluded_params_as_string)
+    private static function generateFunction($route_name,$function_suffix,$excluded_params_as_string,$additional_parameters='')
     {
-        $function_name = $name.'_path';
+        $function_name = $route_name.'_'.$function_suffix;
         if (function_exists($function_name)) return;
+
+        $additional_parameters ? $additional_parameters .= ',' : null;
+        
         $code = <<<BANNER
 function $function_name(\$params=array())
 {
     \$url_writer = AkUrlWriter::getInstance();
     \$my_params = array(
-        'use_named_route'=>'$name',
-        'only_path'=>true,
+        'use_named_route'=>'$route_name',
+        $additional_parameters
         'skip_old_parameters_except'=>$excluded_params_as_string
     );
     \$params = array_merge(\$my_params,\$params);
@@ -54,8 +42,10 @@ function $function_name(\$params=array())
 BANNER;
         #echo $code;
         eval($code);
+        self::$defined_functions[] = $function_name;
         return $code;
     }
+    
 }
 
 ?>
