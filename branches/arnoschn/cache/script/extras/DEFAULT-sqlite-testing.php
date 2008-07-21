45eaf408861e3c18f8779ff5e1c225dfb409e408
arnoschn <arnoschn@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-07-21 21:32:47 +0000

    adding default configs for different backend testing
    git-svn-id: http://svn.akelos.org@908 a2fa5c27-f921-0410-a72c-bf682d381be0

---
+++ branches/arnoschn/cache/script/extras/DEFAULT-sqlite-testing.php
@@ -0,0 +1,24 @@
+<?php
+
+$database_settings = array(
+'testing' => array(
+    'type' => 'postgres',
+    'host' => 'localhost',
+    'database_name' => 'framework_tests',
+    'user' => '',
+    'password' => '',
+    'options' => ''
+));
+
+$database_settings['development'] = $database_settings['production'] = $database_settings['testing'];
+
+defined('AK_ENVIRONMENT') ? null : define('AK_ENVIRONMENT', 'testing');
+defined('AK_BASE_DIR') ? null : define('AK_BASE_DIR', str_replace(DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php','',__FILE__));
+defined('AK_TESTING_URL') ? null : define('AK_TESTING_URL', 'http://localhost/test/fixtures/public');
+define('AK_LOG_EVENTS', true);
+
+include('fix_htaccess.php');
+
+include_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'boot.php');
+
+?>
