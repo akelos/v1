4d26a1e2b0329144f955305beba342c053b9beb7
kaste <kaste@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-07-22 19:57:19 +0000

    Small reorder of routes in test-file.
    git-svn-id: http://svn.akelos.org@920 a2fa5c27-f921-0410-a72c-bf682d381be0

--- branches/kaste/PHPUnit_TestSuite/tests/fixtures/routes.php
+++ branches/kaste/PHPUnit_TestSuite/tests/fixtures/routes.php
@@ -2,10 +2,10 @@
 
 // You can find more about routes on /lib/AkRouters.php and /test/test_AkRouter.php
 
-$Map->connect('/:controller/:action/:id', array('controller' => 'page', 'action' => 'index', 'id'=>OPTIONAL),array('id'=>'\d{1,}'));
 $Map->connect('/', array('controller' => 'page', 'action' => 'index'));
 $Map->connect('/:artist/:album/tags',array('controller'=>'tags'));
 $Map->connect('/admin/logs/:controller/:action/:id',array('module'=>'admin/logs'));
+$Map->connect('/:controller/:action/:id', array('controller' => 'page', 'action' => 'index'),array('id'=>'\d{1,}'));
 $Map->connect('/:module/:controller/:action/:id',array('action'=>COMPULSORY));
 
 ?>
