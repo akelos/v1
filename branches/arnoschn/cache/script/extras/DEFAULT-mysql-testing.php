21c2ae5d6a09aa7a1f55e78074fceefb4cd30803
arnoschn <arnoschn@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-07-23 14:02:39 +0000

    updating default config files for backend testing
    git-svn-id: http://svn.akelos.org@933 a2fa5c27-f921-0410-a72c-bf682d381be0

--- branches/arnoschn/cache/script/extras/DEFAULT-mysql-testing.php
+++ branches/arnoschn/cache/script/extras/DEFAULT-mysql-testing.php
@@ -11,12 +11,13 @@ $database_settings = array(
 ));
 
 $database_settings['development'] = $database_settings['production'] = $database_settings['testing'];
-
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 defined('AK_ENVIRONMENT') ? null : define('AK_ENVIRONMENT', 'testing');
 defined('AK_BASE_DIR') ? null : define('AK_BASE_DIR', str_replace(DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php','',__FILE__));
-defined('AK_TESTING_URL') ? null : define('AK_TESTING_URL', 'http://localhost/test/fixtures/public');
+defined('AK_FRAMEWORK_DIR') ? null : define('AK_FRAMEWORK_DIR', str_replace(DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php',DS.'..'.DS,__FILE__));
+defined('AK_TESTING_URL') ? null : define('AK_TESTING_URL', 'http://localhost:8181/test/fixtures/public');
 define('AK_LOG_EVENTS', true);
-
+defined('AK_TMP_DIR') ? null : define('AK_TMP_DIR', AK_BASE_DIR.DS.'test'.DS.'tmp');
 include('fix_htaccess.php');
 
 include_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'boot.php');
--- branches/arnoschn/cache/script/extras/DEFAULT-postgres-testing.php
+++ branches/arnoschn/cache/script/extras/DEFAULT-postgres-testing.php
@@ -2,7 +2,7 @@
 
 $database_settings = array(
 'testing' => array(
-    'type' => 'sqlite',
+    'type' => 'pgsql',
     'host' => 'localhost',
     'database_file'=>'/tmp/akelos.sqlite',
     'database_name' => 'framework_tests',
@@ -12,10 +12,11 @@ $database_settings = array(
 ));
 
 $database_settings['development'] = $database_settings['production'] = $database_settings['testing'];
-
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 defined('AK_ENVIRONMENT') ? null : define('AK_ENVIRONMENT', 'testing');
 defined('AK_BASE_DIR') ? null : define('AK_BASE_DIR', str_replace(DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php','',__FILE__));
-defined('AK_TESTING_URL') ? null : define('AK_TESTING_URL', 'http://localhost/test/fixtures/public');
+defined('AK_FRAMEWORK_DIR') ? null : define('AK_FRAMEWORK_DIR', str_replace(DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php',DS.'..'.DS,__FILE__));
+defined('AK_TESTING_URL') ? null : define('AK_TESTING_URL', 'http://localhost:8181/test/fixtures/public');
 define('AK_LOG_EVENTS', true);
 
 include('fix_htaccess.php');
