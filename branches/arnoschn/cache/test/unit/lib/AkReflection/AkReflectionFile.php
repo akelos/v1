06a1e7a8462aa91a742f63cd2e6d4638fa2a7fca
arnoschn <arnoschn@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-08-29 11:57:00 +0000

    adding testcase for the wrongly indented docblock
    git-svn-id: http://svn.akelos.org@1055 a2fa5c27-f921-0410-a72c-bf682d381be0

--- branches/arnoschn/cache/test/unit/lib/AkReflection/AkReflectionFile.php
+++ branches/arnoschn/cache/test/unit/lib/AkReflection/AkReflectionFile.php
@@ -24,6 +24,19 @@ class AkReflectionFile_TestCase extends  UnitTestCase
         $this->assertEqual('ReflectionTestClass1',$classes[0]->getName());
         $this->assertEqual('ReflectionTestClass2',$classes[1]->getName());
     }
+    
+    function test_special1()
+    {
+        $filename = AK_TEST_DIR.DS.'fixtures'.DS.'data'.DS.'reflection_doc_block_test_class.php';
+        $file = new AkReflectionFile($filename);
+        $this->assertEqual(1,count($file->getClasses()));
+        $classes = $file->getClasses();
+        $this->assertEqual('ReflectionDocBlockTestClass',$classes[0]->getName());
+        
+        $class = $classes[0];
+        $this->assertEqual('BaseActiveRecord',$class->getTag('ExtensionPoint'));
+
+    }
 }
 
 ak_test('AkReflectionFile_TestCase',true);
